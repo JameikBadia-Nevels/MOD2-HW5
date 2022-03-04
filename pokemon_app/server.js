3c8c6e9f6199743e6fa83ba23ed42497760938b6
JameikBadia-Nevels <jameiknevels@gmail.com>
2022-03-04 15:11:03 -0500

    Database in browser
    Connected my database, can see json in the browser

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -5,10 +5,16 @@ const app = express()
 
 const port = 3000
 
+const pokemon = require('./models/pokemon') 
+
 app.get('/', (req,res)=>{
     res.send('Welcome to the Pokemon App!')
 })
 
+app.get('/pokemon', (req,res) => {
+    res.send(pokemon)
+})
+
 app.listen(port, () =>{
-    console.log("Meh hear erryting ya say", port)
+    console.log("Meh hear erryting ya say in port", port)
 })
